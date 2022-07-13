995f008172b7cb676a58d1e1c9426a20c45e3b29
DuatTran268 <duatsen36@gmail.com>
2022-07-13 20:39:04 +0700

    couse basic by howkteam

---
+++ KteamCourse/bai18groupby.sql
@@ -0,0 +1,55 @@
+﻿/*Lệnh sử dụng cơ sở dữ liệu*/
+use TruyVanKteam
+go
+
+
+/*
+agreeate function
+
+avg()  tính trung bình
+count() Đếm 
+first() 
+last() phần tử cuối cùng
+max() phần tử lớn nhất
+min() phần tử nhỏ nhất
+round() làm tròn
+sum() tính tổng
+
+
+string function
+charindex: tìm một phần tử có tồn tại trong chuỗi ko
+concat() cắt chuỗi
+left() lấy ra bên trái bao nhiêu phần tử
+len() lấy ra độ dài 
+lower() cho thành kiểu viết thường
+ltrim() cắt khoảng trắng
+subtring() lấy ra chuỗi phụ 
+replace() thay thế
+right() lấy bên phải
+rtrimh() cắt khoảng trắng ở bên phải
+upper() cho tất cả viết hoa
+*/
+
+
+-- xuất ra danh sách tên bộ môn và số lượng giáo viên của bộ môn đó
+select TENBM, COUNT(*) from BOMON BM, GIAOVIEN GV
+where BM.MABM = GV.MABM
+group by TENBM
+
+-- column cột hiển thị phải là thuộc tính nằm trong khối grop by hoặc là agreegate functio
+-- group by hiển thị cái gì thì gom nhóm cái đó
+--group by: nhóm cái gì cũng được, gom nhóm theo cái gì đưa vào trong đó, nó chung cái gì là gom lại cái đó 
+
+select TENBM, PHONG, COUNT(*) from BOMON BM, GIAOVIEN GV
+where BM.MABM = GV.MABM
+group by TENBM, BM.MABM, PHONG			-- gom nhóm group by theo tên bộ môn, mã bộ môn và phòng
+
+-- lấy ra danh sách giáo viên có lương lớn hơn lương trung bình của giáo viên
+
+select * from GIAOVIEN
+where LUONG > (select SUM(Luong) from GIAOVIEN)/(select COUNT(*) from GIAOVIEN)
+
+-- xuất ra tên giáo viên và số lượng đề tài giáo viên đó đã làm
+select GV.HOTEN, COUNT(*) from GIAOVIEN GV, THAMGIADT as TGDT
+where GV.MAGV = TGDT.MAGV
+group by GV.MAGV, GV.HOTEN
---
+++ KteamCourse/bai5insertdeleteupdate.sql
@@ -0,0 +1,56 @@
+﻿
+create table test2(
+	MaSo int,
+	Ten nvarchar(10),
+	NgaySinh Date,
+	Nam bit,
+	DiaChi char(20),
+	TienLuong float
+)
+go
+
+-- thêm dữ liệu
+-- kiểu số ghi rõ số bình thường
+-- kiểu ký tự hoặc ngày cần để trong cặp dấu nháy đơn 
+-- nếu là unicode thì cần có N phía trước cặp dấu nháy đơn N''
+
+-- inset dữ liệu theo thứ tự của bảng
+insert dbo.test2 (MaSo, Ten, NgaySinh, Nam, DiaChi, TienLuong)
+values ( 102,	-- MaSo - kiểu int 
+		N'Duật',	-- Tên - nvarchar(10)
+		GETDATE(),	-- ngay sinh - date
+		0,		-- Nam - bit (nam là 1, nữ là 0)
+		'Thanh Hóa',	-- Địa chỉ - char(20)
+		1000000			-- tiền lương - float
+		)
+
+-- xóa dữ liệu
+-- nếu chỉ delete <Tên bảng> => xóa toàn bộ dữ liệu trong bảng
+-- xóa trường mong muốn thêm where
+-- các toán tử >, <, >=, <=, and, or, =
+
+
+-- xóa toàn bộ dữ liệu trong bảng test2
+delete dbo.test2
+
+-- xóa với điều kiện 
+-- vd: xóa thỏa mãn điều kiện có mã số là 12
+delete dbo.test2 where MaSo = 12
+-- xóa tiền lương lớn hơn 5000
+delete dbo.test2 where TienLuong > 5000
+
+-- xóa tiền lương lớn hơn 5000 và có mã số là 10
+delete dbo.test2 where TienLuong > 5000 and MaSo < 15
+
+--=========== update dữ liệu ============
+-- update dữ liệu toàn bộ bảng với 1 trường update
+update dbo.test2 set TienLuong = 2000000
+
+-- update toàn bộ dữ liệu toàn bộ bảng với nhiều trường update
+update dbo.test2 set TienLuong = 100000000, DiaChi = 'Tp HCM'
+
+-- update trường dữ liệu mong muốn
+update dbo.test2 set TienLuong = 368000 where Nam = 1
+
+
+
