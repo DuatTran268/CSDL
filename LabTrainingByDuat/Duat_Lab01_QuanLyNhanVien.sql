ba848319f0c2c3aac84231d25e681a86966ea02b
DuatTran268 <duatsen36@gmail.com>
2022-07-16 10:52:54 +0700

    lab tranning by Duat

---
+++ LabTrainingByDuat/Duat_Lab01_QuanLyNhanVien.sql
@@ -0,0 +1,230 @@
+﻿/*
+	Học phần cơ sở dữ liệu
+	Ngày: 
+	Người thực hiện: Trần Nhật Duật
+	Lớp: CTK44B
+*/
+
+-- lệnh tạo database
+create database Lab01_QuanLyNhanVien
+go 
+-- lệnh sử dụng database
+use Lab01_QuanLyNhanVien
+go
+
+-- tạo các bảng
+-- lệnh tạo bảng Chi Nhánh
+create table ChiNhanh(
+MSCN char(2) primary key,
+TenCN nvarchar(50)
+)
+go
+-- tạo bảng nhân viên
+create table NhanVien(
+MANV char(4) primary key,
+Ho nvarchar(50)  not null,
+Ten nvarchar(10) not null,
+NgaySinh date,
+NgayVaoLam date,
+-- khai báo MSCN là khóa ngoại tham chiếu đến khóa chính MSCN của quan hệ ChiNhanh
+MSCN char(2) references ChiNhanh(MSCN)
+)
+go
+-- tạo bảng kỹ năng
+create table KyNang(
+MSKN char(2) primary key,
+TenKN nvarchar(30)
+)
+go
+-- tạo bảng nhân viên kỹ năng
+create table NhanVienKyNang(
+MANV char(4) references NhanVien(MANV),
+MSKN char(2) references KyNang(MSKN),
+MucDo tinyint check (MucDo between 1 and 9),		-- mức độ thuộc tập trong khoảng từ 1 đến 9
+primary key(MANV, MSKN)		-- khai báo khóa chính gồm nhiều thuộc tính
+)
+go
+
+-- xem các bảng vừa tạo
+select * from ChiNhanh
+select * from NhanVien
+select * from KyNang
+select * from NhanVienKyNang
+
+-- nhập dữ liệu cho bảng 
+insert into ChiNhanh values
+('01', N'Quận 1'),
+('02', N'Quận 5'),
+('03', N'Bình Thạnh')
+-- xem dữ liệu vừa nhập vào bảng
+select * from ChiNhanh
+
+-- nhập dữ liệu cho bảng NhanVien
+set dateformat dmy
+insert into NhanVien values
+('0001', N'Lê Văn', N'Minh', '10/06/1960','02/05/1986', '01'),
+('0002', N'Nguyễn Thị', N'Mai', '20/04/1970','04/07/2001', '01'),
+('0003', N'Lê Anh', N'Tuấn', '25/06/1975','01/09/1982', '02'),
+('0004', N'Vương Tuấn', N'Vũ', '25/03/1960','12/01/1986', '02'),
+('0005', N'Lý Anh', N'Hân', '01/12/1980','15/05/2004', '02'),
+('0006', N'Phan Lê', N'Tuấn', '04/06/1976','25/10/2002', '03'),
+('0007', N'Lê Tuấn', N'Tú', '15/08/1975','15/08/2000', '03')
+-- xem dữ liệu vừa nhập vào bảng
+select * from NhanVien
+
+-- nhập dữ liệu cho bảng KyNang
+insert into KyNang values
+('01', N'Word'),
+('02', N'Excel'),
+('03', N'Access'),
+('04', N'Power Point'),
+('05', N'SPSS')
+-- xem dữ liệu nhập vào
+select * from KyNang
+
+-- nhập dữ liệu cho bảng nhân viên kỹ năng
+insert into NhanVienKyNang values
+('0001', '01', 2),
+('0001', '02', 1),
+('0002', '01', 2),
+('0002', '03', 2),
+('0003', '02', 1),
+('0003', '03', 2),
+('0004', '01', 5),
+('0004', '02', 4),
+('0004', '03', 1),
+('0005', '02', 4),
+('0005', '04', 4),
+('0006', '05', 4),
+('0006', '02', 4),
+('0006', '03', 2),
+('0007', '03', 4),
+('0007', '04', 3)
+select * from NhanVienKyNang
+/*
+delete tenbang: xóa dữ liệu trong bảng
+vd: delete NhanVienKyNang
+*/
+
+-- truy vấn dữ liệu -------------
+-- 1 truy vấn lựa chọn trên nhiều bảng
+-- a> Hiển thị msnv , họ teenm số năm làm việc
+/*
+cách tính số năm làm việc: sử dụng hàm YEAR(năm hiện tại) - YEAR(năm của biến ngày vào làm)
+- hàm GETDATE: lấy ngày giờ hiện tại
+*/
+select MANV, Ho + ' ' + Ten as HoTen, YEAR(GETDATE()) - YEAR(NgayVaoLam) as SoNamLamViec  
+from NhanVien a
+where YEAR(GETDATE()) - YEAR(NgayVaoLam) > 20
+
+--b.  Liệt kê các thông tin về nhân viên: HoTen, NgaySinh, NgayVaoLam, TenCN(sap xep theo ten chi nhanh)
+select Ho + ' ' + Ten as HoTen, NgaySinh, NgayVaoLam, TenCN 
+from NhanVien a, ChiNhanh b
+where a.MSCN = b.MSCN
+order by TenCN
+
+-- c. Liệt kê các nhân viên (HoTen, TenKN, MucDo) của những nhân viên biết sử dụng word
+select Ho + ' ' + Ten as HoTen, c.TenKN, MucDo 
+from NhanVien a, NhanVienKyNang b, KyNang c
+where a.MANV = b.MANV and b.MSKN = c.MSKN and TenKN = N'Word'
+
+--d. Liệt kê các kỹ năng (TenKN, MucDo) mà nhân viên 'Lê Anh Tuấn' biết sử dụng
+select TenKN, MucDo
+from NhanVien a, NhanVienKyNang b, KyNang c
+where a.MANV = b.MANV and b.MSKN = c.MSKN and a.Ho = N'Lê Anh' and a.Ten = N'Tuấn' 
+
+-- 2. truy vấn lồng
+--a. liệt kê MANV, HoTen, MSCN, TenCN của các nhân viên có mức độ thành thạo về 'Excel' cao nhất trong công ty
+select a.MANV, Ho + ' ' + Ten as HoTen, TenCN, MucDo 
+from NhanVien a, ChiNhanh b, NhanVienKyNang c, KyNang d
+where a.MSCN = b.MSCN and a.MANV = c.MANV and c.MSKN = d.MSKN and TenKN = N'Excel'
+and c.MucDo = (
+	select MAX(e.MucDo)		-- vì tìm mức độ cao nhất mà mức độ nằm ở bảng nhân viên kỹ năng nên => e.MucDo
+	from NhanVienKyNang e, KyNang f
+	where e.MSKN = f.MSKN and f.TenKN = N'Excel'
+)
+-- làm ngược: --a. liệt kê MANV, HoTen, MSCN, TenCN của các nhân viên có mức độ thành thạo về 'Excel' thấp nhất trong công ty
+select a.MANV, Ho + ' ' + Ten as HoTen,  TenCN, MucDo
+from NhanVien a, ChiNhanh b, KyNang c, NhanVienKyNang d
+where a.MSCN = b.MSCN and a.MANV = d.MANV and c.MSKN = d.MSKN and TenKN = N'Excel'
+and d.MucDo = (	-- truy vấn lồng tìm ra mức độ thấp nhất 
+	select MIN(f.MucDo) 
+	from KyNang e, NhanVienKyNang f
+	where e.MSKN = f.MSKN and e.TenKN = N'Excel'
+)
+
+-- b. Liệt kê MANV, HoTen, TenCN của các nhân viên vừa biết word vừa biết excel
+/*
+b1: tìm ra các nhân viên biết sử dụng Execl trong bảng
+b2: dựa vào a.MaNV mã nhân viên trong bảng nhân viên tìm xem tại cái mã nhân viên đó
+b3: tìm ra các nhân viên biết sử dụng word thông qua mã nhân viên của bảng nhân viên
+*/
+
+select a.MANV, Ho + ' '+ Ten	as HoTen, TenCN
+from NhanVien a, ChiNhanh b, KyNang c, NhanVienKyNang d
+where a.MSCN = b.MSCN and a.MANV = d.MANV and c.MSKN = d.MSKN and TenKN = N'Excel'
+and a.MANV in (
+	select f.MANV 
+	from KyNang  e, NhanVienKyNang f
+	where e.MSKN = f.MSKN and e.TenKN = N'Word'
+)
+-- b. Liệt kê MANV, HoTen, TenCN của các nhân viên vừa biết access vừa biết power point
+select a.MANV, Ho + ' ' + Ten as HoTen, TenCN 
+from NhanVien a, ChiNhanh b, KyNang c, NhanVienKyNang d
+where a.MANV = d.MANV and a.MSCN = b.MSCN and c.MSKN = d.MSKN and c.TenKN = N'Access'
+and a.MANV in (
+	select f.MANV
+	from KyNang e, NhanVienKyNang f
+	where e.MSKN = f.MSKN and e.TenKN = N'Power Point'
+)
+
+--c. Với từng kỹ năng, liệt kê các thong tin (MaNV, HoTen, TenCN, TenKN, MucDo) của những nhân viên thành thạo kỹ năng đó nhất
+/*
+-- muốn tìm kỹ năng thành thạo nhất dựa vào mức độ thành thạo trong bảng nhân viên kỹ năng
+*/
+select a.MANV, Ho + ' ' + Ten as HoTen, TenCN, TenKN, MucDo
+from NhanVien a, ChiNhanh b, KyNang c, NhanVienKyNang d
+where a.MANV = d.MANV and a.MSCN = b.MSCN and c.MSKN = d.MSKN 
+and d.MucDo = (
+	select MAX(e.MucDo)
+	from NhanVienKyNang e
+	where e.MSKN = c.MSKN
+)
+group by a.MANV, Ho + ' ' + Ten,TenCN, TenKN, MucDo
+
+-- làm tương tự câu c mà chỉ có nhân viên thành thạo kỹ năng đó nhất trong các nhân viên
+select a.MANV, Ho + ' ' + Ten as HoTen, TenKN, TenCN, MucDo 
+from NhanVien a, ChiNhanh b, KyNang c, NhanVienKyNang d
+where a.MANV = d.MANV and a.MSCN = b.MSCN and c.MSKN = d.MSKN 
+and d.MucDo = (
+	select Min(e.MucDo) 
+	from NhanVienKyNang e, KyNang f
+	where e.MSKN = f.MSKN
+)
+group by a.MANV, Ho + ' ' + Ten, TenKN, TenCN, MucDo 
+
+-- d. liệt kê các chi nhánh(MSCN, TenCN) mà mọi nhân viên trong chi nhánh đó đều biết 'Word'
+select distinct b.MSCN, TenCN
+from NhanVien a, ChiNhanh b, KyNang c, NhanVienKyNang d
+where a.MANV = d.MANV and a.MSCN = b.MSCN and c.MSKN = d.MSKN and c.TenKN = N'Word'
+
+-- 3. truy vấn gom nhóm dữ liệu
+-- a. với mỗi chi nhánh, hãy cho biết các thông tin sau TenCN, SoNV(số nhân viên)
+select b.MSCN, TenCN, COUNT(MANV) as SoNV 
+from NhanVien a, ChiNhanh b
+where a.MSCN = b.MSCN
+group by  TenCN , b.MSCN
+
+-- b, với mỗi kỹ năng hãy cho biết TenKH, SoNguoiDung(Số nhân viên biết sử dụng kỹ năng đó )
+select b.TenKN, COUNT(a.MANV) as SoNguoiBietDung
+from NhanVien a, KyNang b, NhanVienKyNang c
+where a.MANV = c.MANV and c.MSKN = b.MSKN
+group by TenKN
+
+-- c. Cho biết TenKN có từ 3 nhân viên trong cty sử dụng trở lên
+select b.TenKN, COUNT(a.MANV) as SoNguoiBietDung
+from NhanVien a, KyNang b, NhanVienKyNang c
+where a.MANV = c.MANV and c.MSKN = b.MSKN
+group by TenKN
+
+-- d cho biết tên chi nhánh có nhiều nhân viên nhất
---
+++ LabTrainingByDuat/Duat_Lab06_QLNhanVien.sql
@@ -0,0 +1,214 @@
+﻿/*
+Học phần cơ sở dữ liệu
+Người thực hiện: Trần Nhật Duật
+Lớp CTK44B
+Mssv: 2012254
+*/
+
+create database Lab06_QLHocVien
+go
+use Lab06_QLHocVien
+go
+
+create table CaHoc(
+Ca tinyint primary key,		-- kiểu dữ liệu tinyint: kích thước từ 0 -> 255
+GioBatDau datetime,
+GioKetThuc datetime
+)
+go
+create table GiaoVien(
+MSGV char(4) primary key,
+HoGV nvarchar(20),
+TenGV nvarchar(10),
+DienThoai varchar(11)
+)
+go
+create table Lop(
+MaLop char(4) primary key,
+TenLop nvarchar(30),
+NgayKG datetime,
+HocPhi int,
+Ca tinyint references CaHoc(Ca),
+SoTiet int,
+SoHV int,
+MSGV char(4) references GiaoVien(MSGV)
+)
+go
+create table HocVien(
+MSHV char(4) primary key,
+Ho nvarchar(30),
+Ten nvarchar(10),
+NgaySinh datetime,
+Phai nvarchar(4),
+MaLop char(4) references Lop(MaLop)
+)
+go
+create table HocPhi(
+SoBL char(6) primary key,
+MSHV char(4) references HocVien(MSHV),
+NgayThu datetime,
+SoTien int,
+NoiDung nvarchar(50),
+NguoiThu nvarchar(30)
+)
+go
+
+--drop table HocPhi	-- lệnh xóa bảng
+
+-- xem các bảng vừa tạo
+select * from CaHoc
+select * from GiaoVien
+select * from Lop
+select * from HocVien
+select * from HocPhi
+
+-- xây dựng các thủ tục nhập dữ liệu vào các bảng
+create proc usp_CaHoc
+@ca tinyint,
+@giobd datetime,
+@giokt datetime
+as	
+-- exists: tồn tại điều kiện -> nếu tồn tại điều kiện-> thì
+	if exists (select * from CaHoc where Ca = @ca) -- kiểm tra xem có trùng khóa chính Ca
+		print N'Đã có ca học ' + @ca + N' trong CSDL'
+	else
+		begin
+			insert into CaHoc values (@ca, @giobd, @giokt)
+			print N'Thêm ca học thành công'
+		end
+go
+-- gọi thực hiện thủ tục usp_CaHoc
+exec usp_CaHoc 1,'7:30', '10:45'
+exec usp_CaHoc 2,'13:30', '16:45'
+exec usp_CaHoc 3,'17:30', '20:45'
+select * from CaHoc
+
+-- thêm dữ liệu vào bảng giáo viên
+create proc usp_GiaoVien
+@magv char(4),
+@hogv nvarchar(20),
+@tengv nvarchar(10),
+@dienthoai varchar(11)
+as
+	if exists (select * from GiaoVien where MSGV = @magv)	-- kiểm tra khóa chính nếu có trùng lặp lại xuất ra thông báo
+		print N'Đã tồn tại Mã GV ' + @magv + N' trong CSDL'
+	else
+		begin
+			insert into GiaoVien values (@magv, @hogv, @tengv, @dienthoai)
+			print N'Thêm giáo viên thành công'
+		end
+go
+
+exec usp_GiaoVien 'G001',N'Lê Hoàng',N'Anh', '858936'
+exec usp_GiaoVien 'G002',N'Nguyễn Ngọc',N'Lan', '845623'
+exec usp_GiaoVien 'G003',N'Trần Minh',N'Hùng', '823456'
+exec usp_GiaoVien 'G004',N'Võ Thanh',N'Trung', '841256'
+select * from GiaoVien
+
+-- nhập dữ liệu vào bảng lớp
+create proc usp_Lop
+@malop char(4),
+@tenlop nvarchar(30),
+@ngaykg datetime,
+@hocphi int,
+@ca tinyint,		-- khóa ngoại ca tham chiếu đến khóa chính trong bảng CaHoc
+@sotiet int,
+@sohv int,
+@msgv char(4)		-- khóa ngoại msgv tham chiếu đến khóa chính trong bảng GiaoVien
+as
+	-- kiểm tra các ràng buộc toàn vẹn khóa ngoại
+	if exists (select * from CaHoc where Ca = @ca) and exists (select * from GiaoVien where MSGV = @msgv)
+		begin
+			if exists (select * from Lop where MaLop = @malop)	-- kiểm tra có trùng khóa chính của quan hệ Lớp
+				print N'Đã có mã lớp ' + @malop + ' trong CSDL'
+			else
+				begin
+					insert into Lop values (@malop, @tenlop, @ngaykg, @hocphi, @ca, @sotiet, @sohv, @msgv)
+					print N'Thêm dữ liệu vào bảng lớp học thành công'
+				end
+		end
+	else
+		print N'Lớp ' + @malop + N' không tồn tại trong CSDL nên không thể thêm học viên vào lớp này'
+go
+-- gọi thực hiện thủ tục usp_Lop
+set dateformat dmy
+go
+exec usp_Lop 'A075',N'Access 2-4-6','18/12/2008', 150000,3,60,3,'G003'
+exec usp_Lop 'E114',N'Excel 3-5-7','02/01/2008', 120000,1,45,3,'G003'
+exec usp_Lop 'A115',N'Excel 2-4-6','22/01/2008', 120000,3,45,0,'G001'
+exec usp_Lop 'W123',N'Word 2-4-6','18/02/2008', 100000,3,30,1,'G001'
+exec usp_Lop 'W124',N'Word 3-5-7','01/03/2008', 100000,1,30,0,'G002'
+-- xem dữ liệu bảng vừa nhập
+select * from Lop
+
+-- nhập dữ liệu cho bảng học viên
+create proc usp_HocVien
+@mshv char(4),
+@ho nvarchar(30),
+@ten nvarchar(10),
+@ngaysinh datetime,
+@phai nvarchar(4),
+@malop char(4)	-- khóa ngoại mã lớp tham chiếu đến khóa chính mã lớp ở bảng Lớp
+as
+	-- kiểm tra có ràng buộc toàn vẹn khóa ngoại
+	if exists (select * from Lop where MaLop = @malop)
+		begin 
+			if exists (select * from HocVien where MSHV = @mshv)
+				print N'Đã tồn tại mã học viên ' + @mshv + N' trong CSDL'
+			else
+				begin
+					insert into HocVien values (@mshv, @ho, @ten, @ngaysinh, @phai, @malop)
+					print N'Thêm học viên thành công'
+			end
+		end
+	else
+		print N'Lớp ' + @malop + N' không tồn tại trong CSDL'
+go
+-- gọi thực hiện thủ tục
+set dateformat dmy
+go
+exec usp_HocVien '0001',N'Lê Văn', N'Minh', '10/06/1988',N'Nam', 'A075'
+exec usp_HocVien '0002',N'Nguyễn Thị', N'Mai', '20/04/1988',N'Nữ', 'A075'
+exec usp_HocVien '0003',N'Lê Ngọc', N'Tuấn', '10/06/1984',N'Nam', 'A075'
+exec usp_HocVien '0004',N'Vương Tuấn', N'Vũ', '25/03/1979',N'Nam', 'E114'
+exec usp_HocVien '0005',N'Lý Ngọc', N'Hân', '01/12/1985',N'Nữ', 'E114'
+exec usp_HocVien '0006',N'Trần Mai', N'Linh', '04/06/1980',N'Nữ', 'E114'
+exec usp_HocVien '0007',N'Nguyễn Ngọc', N'Tuyết', '12/05/1986',N'Nữ', 'W123'
+select * from HocVien
+
+-- nhập dữ liệu cho bảng học phí
+create proc usp_HocPhi
+@sobl char(6),
+@mshv char(4),
+@ngaythu datetime,
+@sotien int,
+@noidung nvarchar(50),
+@nguoithu nvarchar(30)
+as
+	-- kiểm tra ràng buộc toàn vẹn khóa ngoại
+	if exists (select * from HocVien where MSHV = @mshv)
+		begin
+			if exists (select * from HocPhi  where SoBL = @sobl)	-- kiểm tra xem có trùng khóa (số biên lai)
+				print N' Đã tồn tại số biên lai ' + @sobl + N' trong CSDL'
+			else
+				begin
+					insert into HocPhi values (@sobl, @mshv, @ngaythu, @sotien, @noidung, @nguoithu)
+					print N'Thêm dữ liệu vào bảng Học phí thành công'
+				end
+		end
+	else
+		print N'Học viên ' + @mshv + N' không tồn tại bên trong CSDL'
+go
+----goi thuc hien thu tuc usp_ThemHocPhi-------
+set dateformat dmy
+go
+exec usp_HocPhi 'A07501','0001','16/12/2008',150000,'HP Access 2-4-6', N'Lan'
+exec usp_HocPhi 'A07502','0002','16/12/2008',100000,'HP Access 2-4-6', N'Lan'
+exec usp_HocPhi 'A07503','0003','18/12/2008',150000,'HP Access 2-4-6', N'Vân'
+exec usp_HocPhi 'A07504','0002','15/01/2009',50000,'HP Access 2-4-6', N'Vân'
+exec usp_HocPhi 'E11401','0004','02/01/2008',120000,'HP Excel 3-5-7', N'Vân'
+exec usp_HocPhi 'E11402','0005','02/01/2008',120000,'HP Excel 3-5-7', N'Vân'
+exec usp_HocPhi 'E11403','0006','02/01/2008',80000,'HP Excel 3-5-7', N'Vân'
+exec usp_HocPhi 'W12301','0007','18/02/2008',100000,'HP Word 2-4-6', N'Lan'
+		
+select * from HocPhi
